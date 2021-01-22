de4350c59236c809d64c796e5c156c49f88aab72
Dayne Dewar <daynedewar@yahoo.com>
2021-01-22 09:27:22 -0500

    commit tree 1c0a4ded51fe4bef46d5d31ee73e25e82b007506

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,14 +3,18 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    # return the number of songs in a genre
+    self.songs.count
+  end
+
+  def songs_same_genre
+    songs.select {|song| song.genre == self}
   end
 
   def artist_count
-    # return the number of artists associated with the genre
+    songs_same_genre.map(&:artist).uniq.count
   end
 
   def all_artist_names
-    # return an array of strings containing every musician's name
+    songs.map {|song| song.artist.name} 
   end
 end
